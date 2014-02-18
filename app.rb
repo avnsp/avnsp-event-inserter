5d3a66900f9e52072dd941091d3a0f807099dd8f
Magnus Hörberg <magnus.hoerberg@gmail.com>
2014-02-18 19:14:23 +0100

    fix code style

--- app.rb
+++ app.rb
@@ -12,17 +12,17 @@ TH.with_channel prefetch: 100 do |ch|
   ch.subscribe 'event.photo.create', 'photo.uploaded' do |data|
     evt = { name: 'photo', data: data.to_json }
     id = DB[:events].insert(evt)
-    ch.publish 'event.photo.created', evt.merge id: id
+    ch.publish 'event.photo.created', evt.merge(id: id)
   end
   ch.subscribe 'event.member.create', 'member.created' do |data|
     evt = { name: 'member', data: data.to_json }
     id = DB[:events].insert(evt)
-    ch.publish 'event.member.created', evt.merge id: id
+    ch.publish 'event.member.created', evt.merge(id: id)
   end
   ch.subscribe 'event.party.create', 'party.created' do |data|
     evt = { name: 'party', data: data.to_json }
     id = DB[:events].insert(evt)
-    ch.publish 'event.party.created', evt.merge id: id
+    ch.publish 'event.party.created', evt.merge(id: id)
   end
 end
 puts "[INFO] AVNSP event instert handler starting..."
